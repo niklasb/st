70c882a77a6790e9e94edf813e760a4de51e5f16
Niklas Baumstark <niklas.baumstark@gmail.com>
2020-01-19 10:15:00 +0100

    fix

--- contracts/example.sol
+++ contracts/example.sol
@@ -1,19 +1,22 @@
 
+
     struct TokenBatch {
         uint256 amount;
         uint64 withdrawalDeadline;
     }
+
     struct Holder {
         address addr;
         uint256 reclaimableAmount; // Amount (in EUR) the holder is owed
         TokenBatch[] batches;
     }
+
     mapping(address => Holder);
 
-    function withdrawFromContract(uint256 batchIndex) {
+    function withdrawFromContract(uint256 batchNumber) {
         // Find data structures in memory
         Holder storage holder = holders[msg.sender];
-        TokenBatch storage batch = holder[batchIndex].batches;
+        TokenBatch storage batch = holder.batches[batchNumber];
         // Check the withdrawal deadline against current time
         require(now <= batch.withdrawalDeadline);
         // Return EUR
